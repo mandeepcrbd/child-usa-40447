61a667d1ced4083f858ef73594e12de19a11d0fc
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-07-25 18:30:41 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "calm-fire-40447",
   url: "https://calm-fire-40447.botics.co",
   api: "https://calm-fire-40447.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
